9550a970ad6360a7ac48bce9c2187ac2f467d18d
ebectar <acebe424@gmail.com>
2018-10-22 12:59:09 -0600

    initial commit

---
+++ src/Cake.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Cake = () => {
+  return (
+    <div className="cake">
+      <div className="plate" />
+      <div className="layer layer-bottom" />
+      <div className="layer layer-middle" />
+      <div className="layer layer-top" />
+      <div className="icing" />
+      <div className="drip drip1" />
+      <div className="drip drip2" />
+      <div className="drip drip3" />
+      <div className="candle candle1">
+      g
+        <div className="flame" />
+        <div className="flame inner-flame" />
+      </div>
+      <div className="candle candle2">
+      9
+        <div className="flame" />
+        <div className="flame inner-flame" />
+      </div>
+      <div className="candle candle3">
+      9
+        <div className="flame" />
+        <div className="flame inner-flame" />
+      </div>
+    </div>
+  );
+};
+
+export default Cake;
